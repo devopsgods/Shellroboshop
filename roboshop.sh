e137f5ebb2a3fc51aa22b698e2e51ded45f6ee17
Devils <devopsdevil9gmail.com>
2026-02-15 11:10:51 +0000

    attempt2

--- roboshop.sh
+++ roboshop.sh
@@ -11,7 +11,7 @@ do
         --security-group-ids $SG_ID \
         --instance-type "t3.micro" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
-        --query 'Reservation[0]InstancesId' \
+        --query 'Instance[0]InstancesId' \
         --output text
 
         if[$instance == "frontend"];
