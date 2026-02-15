8d3f14a5df185221459a55bb6048d353186cda75
Devils <devopsdevil9gmail.com>
2026-02-15 14:29:04 +0000

    attempt2

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USER_ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGS_FILE="/tmp/$0-$TIMESTAMP.log"
 LOGS_FOLDER="/var/log/Shellroboshop"
@@ -29,45 +29,6 @@ VALIDATE(){
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mongo repo"
-
-dnf install mongodb-org -y &>>$LOGS_FILE
-VALIDATE $? "Installing MongoDB server"
-
-systemctl enable mongod &>>$LOGS_FILE
-VALIDATE $? "Enable MongoDB"
-
-systemctl start mongod
-VALIDATE $? "Start MongoDB"
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf
-#!/bin/bash
-
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
-    exit 1
-fi
-
-mkdir -p $LOGS_FOLDER
-
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    fi
-}
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying Mongo Repo" 
 
 dnf install mongodb-org -y &>>$LOGS_FILE
 VALIDATE $? "Installing MongoDB server"
