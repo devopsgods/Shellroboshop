be4ce534d7ec16e6b816245c8d572920513c2f0e
Devils <devopsdevil9gmail.com>
2026-02-15 11:45:48 +0000

    attempt2

--- geminiscript.sh
+++ geminiscript.sh
@@ -2,6 +2,8 @@
 
 SG_ID="sg-059bc57f32dfd1979"
 AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z015439937GBQIS91RBN2"
+DOMAIN_NAME="karegowdra.com"
 
 # Using 'i' for the loop variable to keep it simple
 for i in $@
@@ -25,12 +27,39 @@ do
             --instance-ids "$INSTANCE_ID" \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text)
+            recordname="$DOMAIN_NAME"
     else 
         IP=$(aws ec2 describe-instances \
             --instance-ids "$INSTANCE_ID" \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text)
+            recordname="$instance.$DOMAIN_NAME"
     fi
      
     echo "IP Address for $i: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id  $ZONE_ID \
+    --change-batch '
+        {
+        "Comment": "Updating the A record for my EC2 instance",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$recordname'",
+                "Type": "A",
+                "TTL": ,
+                "ResourceRecords": [
+                {
+                    "Value": "'172.31.22.151'"
+                }
+                ]
+            }
+            }
+        ]
+        }
+
+        '
+        echo "" record iupdated for $i
 done
