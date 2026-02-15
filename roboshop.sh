eab1b2664b5a723191de0d43d92ea1180ab48386
Devils <devopsdevil9gmail.com>
2026-02-15 11:12:42 +0000

    attempt2

--- roboshop.sh
+++ roboshop.sh
@@ -11,16 +11,15 @@ do
         --security-group-ids $SG_ID \
         --instance-type "t3.micro" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
-        --query 'Instance[0]InstancesId' \
+        --query 'Instance[0].InstancesId' \
         --output text
 
-        if[$instance == "frontend"];
-
-        then 
-        IP=$( aws ec2 describe-instances \
-         --instance-ids $instance_id \
-         --query 'Reservations[0].Instance[].PublicIpAddress' \
-         --output text
+        if[$instance == "frontend"]; then 
+            IP=$( 
+                aws ec2 describe-instances \
+                --instance-ids $instance_id \
+                --query 'Reservations[0].Instance[].PublicIpAddress' \
+                --output text
 )
 else (
         aws ec2 describe-instances \
