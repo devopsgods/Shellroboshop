654f1516cf3401ea1a2ad425569e520d540daba4
Devils <devopsdevil9gmail.com>
2026-02-15 11:01:50 +0000

    attempt2

---
+++ roboshop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SG_ID="sg-059bc57f32dfd1979"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for INSTANCE IN $@
+do
+    instance_id = $( 
+        aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --security-group-ids $SG_ID \
+        --instance-type "t3.micro" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
+        --query 'Reservation[0]Instances[0].PrivateIpAddress' \
+        --output text
+
+        if[$instance == "frontend"];
+
+        then 
+        IP=$( aws ec2 describe-instances \
+         --instance-ids $instance_id \
+         --query 'Reservations[0].Instance[].PublicIpAddress' \
+         --output text
+)
+else (
+        aws ec2 describe-instances \
+         --instance-ids $instance_id \
+         --query 'Reservations[0].Instance[].PrivateIpAddress' \
+         --output text
+)
+
+fi
+
+    )
+done
