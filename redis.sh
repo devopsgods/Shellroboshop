e7ba94b08c5b26137eb8d8bffe091eb0b2222249
Devils <devopsdevil9gmail.com>
2026-02-21 20:42:27 +0000

    attempt2

---
+++ redis.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGS_FILE="/tmp/$0-$TIMESTAMP.log"
+LOGS_FOLDER="/var/log/Shellroboshop"
+LOGS_FILES="$LOGS_FOLDER/0.log"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.karegowdra.com
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USER_ID -ne 0 ]; then
+    echo -e "$R run with root user access $N" | tee -a $LOGS_FILES
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...... $N failed" | tee -a $LOGS_FILES
+        exit 1
+        else
+        echo -e "$2 .......$N" GREAT SUCCESS| tee -a $LOGS_FILES
+    fi
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
+
+dnf install redis -y  &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
